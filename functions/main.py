bde4cd28fa58bf827ada3bb29d1500a60358168a
thu0310 <thu.nguyen@vanderbilt.edu>
2023-09-26 12:13:17 -0500

    Push again

--- functions/main.py
+++ functions/main.py
@@ -7,6 +7,26 @@ from firebase_admin import initialize_app
 
 initialize_app()
 
+from firebase_functions import https as https_fn
+from firebase_functions import identity as identity_fn
+
+# Block account creation with any non-acme email address.
+@identity_fn.before_user_created()
+def validatenewuser(
+    event: identity_fn.AuthBlockingEvent,
+) -> identity_fn.BeforeCreateResponse | None:
+    # User data passed in from the CloudEvent.
+    user = event.data
+
+    # Only users of a specific domain can sign up.
+    if user.email is None or "@vanderbilt.edu" not in user.email:
+        # Return None so that Firebase Auth rejects the account creation.
+        raise https_fn.HttpsError(
+            code=https_fn.FunctionsErrorCode.INVALID_ARGUMENT,
+            message="Unauthorized email",
+        )
+
+
 @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
     return https_fn.Response("Hello world!")
