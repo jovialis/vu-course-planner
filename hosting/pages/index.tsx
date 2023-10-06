cd3b9568cd5e504e0d5105f4f2ebaa9839e12298
thu0310 <thu.nguyen@vanderbilt.edu>
2023-10-06 14:52:45 -0500

    Accidentally did Expanded Course Info Stuff here

--- hosting/pages/index.tsx
+++ hosting/pages/index.tsx
@@ -4,14 +4,16 @@
  * Author: jovialis (Dylan Hanson)
  * Date: 9/6/23
  */
-import { UserLoginGate } from "../components/UserLoginGate";
+import {UserLoginGate} from "../components/UserLoginGate";
 import ImageDisplay from "../components/Header";
+import {CourseInfo} from "../components/CourseInfo";
 
 export default function HomePage() {
-  return (
-    <>
-      <ImageDisplay imagePath="/images/vuplannerbanner.png" altText=""/>
-      <UserLoginGate>Hello, world!</UserLoginGate>
-    </>
-  );
+    return (
+        <>
+            <ImageDisplay imagePath="/images/vuplannerbanner.png" altText=""/>
+            <CourseInfo/>
+            <UserLoginGate>Hello, world!</UserLoginGate>
+        </>
+    );
 }
