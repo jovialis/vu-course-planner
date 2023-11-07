402365c42fb428f5e55e7f5992c6b89369c3b468
Dylan Hanson <cdh.create@gmail.com>
2023-11-07 08:51:22 -0600

    Wrote course dependency script + testing

---
+++ functions/tests/test_warehouse_requisites.py
@@ -0,0 +1,8 @@
+
+
+def test__warehouse_requisites():
+    from src.warehousing.warehouse_requisites import __structure_course_requisites
+
+    print(__structure_course_requisites("CS 1101 or CS 1103"))
+
+    pass
