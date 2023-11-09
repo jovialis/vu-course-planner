7c9bd6343dfa749a3bdb7116eb609b2a7f94eb59
Dylan Hanson <cdh.create@gmail.com>
2023-11-09 12:11:14 -0600

    Set up testing

---
+++ functions/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python -m pytest tests/test_inclass.py
+
+# # Replace the line above with one similar to this if you only want to run your tests
+# python -m pytest tests/test_inclass.py
--- hosting/components/dashboard/CourseCard.test.tsx
+++
@@ -1,6 +0,0 @@
-import {CourseCard} from "./CourseCard";
-import {it} from "node:test";
-
-it('should return true', () => {
-    expect(true)
-}); 
